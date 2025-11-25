84388620ee42afe2d929b2ac6454934e251a2dfe
Mohammad Kaif Khan <mdkaifk534@gmail.com>
2025-11-25 06:21:31 +0000

    add problem

---
+++ recursion/loop.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    for(int i=5; i>=1; i--){
+        cout<<i<<"days left your birthdays";
+        cout<<endl;
+    }
+    cout<<"Finally Happy Birthdays to you";
+}
